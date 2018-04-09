654b17d58ea4043137f62ad0eeda1bb555e0333e
Kata1213 <2726456039@qq.com>
2018-04-09 15:31:12 +0800

    p3 p4

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,27 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    String basicIntoduce(){
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String introduce() {
+        return this.basicIntoduce();
+    }
 }
